54267f6cd8d29ec6089d7ae64ffef6ddc76239b4
Samarth Hattangady <samhattangady@gmail.com>
2018-05-21 14:22:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pyqtlet',
   packages = ['pyqtlet'],
-  version = '0.2.beta',
+  version = '0.2.beta1',
   description = 'Using leaflet maps in PyQt',
   author = 'Samarth Hattangady',
   author_email = 'samhattangady@gmail.com',
   url = 'https://github.com/skylarkdrones/pyqtlet',
-  download_url = 'https://github.com/skylarkdrones/pyqtlet/archive/0.2.beta.tar.gz',
+  download_url = 'https://github.com/skylarkdrones/pyqtlet/archive/0.2.beta1.tar.gz',
   keywords = ['leaflet', 'pyqt', 'maps'],
   classifiers = [],
 )
