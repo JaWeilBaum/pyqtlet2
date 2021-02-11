c1611a52b8008f2fa3525f1c46f71fa49a18e952
Leon Friedmann <leon@friedmann.eu>
2021-02-11 20:39:42 +0100

    Added 2 events to the marker, 'move' and 'moveend'

--- pyqtlet/leaflet/layer/marker/marker.py
+++ pyqtlet/leaflet/layer/marker/marker.py
@@ -1,11 +1,30 @@
 from ..layer import Layer
 
+from PyQt5.QtCore import pyqtSlot, pyqtSignal, QJsonValue
+
 class Marker(Layer):
+    moveend = pyqtSignal(dict)
+    move = pyqtSignal(dict)
+
     def __init__(self, latLng, options=None):
         super().__init__()
         self.latLng = latLng
         self.options = options
         self._initJs()
+        self._connectEventToSignal('move', '_onMove')
+        self._connectEventToSignal('moveend', '_onMoveend')
+
+    @pyqtSlot(QJsonValue)
+    def _onMove(self, event):
+        self._logger.debug('map clicked. event: {event}'.format(event=event))
+        event = self._qJsonValueToDict(event)
+        self.latLng = [event["latlng"]["lat"], event["latlng"]["lng"]]
+        self.move.emit(event)
+
+    @pyqtSlot(QJsonValue)
+    def _onMoveend(self, event):
+        self._logger.debug('map clicked. event: {event}'.format(event=event))
+        self.moveend.emit({**self._qJsonValueToDict(event), "sender": self})
 
     def _initJs(self):
         leafletJsObject = 'L.marker({latLng}'.format(latLng=self.latLng)
