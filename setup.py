d76e325b7cea5e5fc7f3ab296d1675d89ccacc74
Leon Friedmann <leon.friedmann@tum.de>
2021-09-09 18:11:02 +0200

    Setup.py added

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+OPTIONS={
+    "py2app": {
+        "resources": ["web"]
+    }
+}
+
+setup(
+    name='pyqtlet2',
+    app=["playground.py"],
+    version='0.3.4',
+    description='Bringing leaflet maps to PyQt',
+    author='Leon Friedmann',
+    author_email='leon.friedmann@tum.de',
+    options=OPTIONS,
+    url='https://github.com/JaWeilBaum/pyqtlet2',
+    keywords=['leaflet', 'pyqt', 'maps', 'python', 'python3'],
+    classifiers=[],
+)
