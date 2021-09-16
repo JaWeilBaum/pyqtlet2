58ea5f7c082d2be740d2759c143605231b057c3e
Leon Friedmann <leon.friedmann@tum.de>
2021-09-16 13:19:38 +0200

    Changes to version and setup.py

--- pyqtlet2/__init__.py
+++ pyqtlet2/__init__.py
@@ -2,8 +2,8 @@
 Bringing Leaflet maps to PyQt.
 """
 
-__author__ = 'Samarth Hattangady <samarth@skylarkdrones.com>'
-__version__ = '0.3.3'
+__author__ = 'Leon Friedmann <leon.friedmann@tum.de>'
+__version__ = '0.4.2'
 
 from .mapwidget import MapWidget
 from .leaflet import L
--- setup.py
+++ setup.py
@@ -8,12 +8,13 @@ OPTIONS={
 
 setup(
     name='pyqtlet2',
-    app=["playground.py"],
-    version='0.4.0',
+#    app=["playground.py"],
+    version='0.4.2',
     description='Bringing leaflet maps to PyQt',
     author='Leon Friedmann',
     author_email='leon.friedmann@tum.de',
     options=OPTIONS,
+    package_dir={"": "pyqtlet2"},
     url='https://github.com/JaWeilBaum/pyqtlet2',
     keywords=['leaflet', 'pyqt', 'maps', 'python', 'python3'],
     classifiers=[],
