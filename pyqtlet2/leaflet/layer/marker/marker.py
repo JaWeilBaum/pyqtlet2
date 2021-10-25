5933fd97b54381f3b1855b3e5f27446424abced9
Leon Friedmann <leon@friedmann.eu>
2021-10-25 10:36:27 +0200

    Changes to marker.py
    Signed-off-by: Leon Friedmann <leon@friedmann.eu>

--- pyqtlet2/leaflet/layer/marker/marker.py
+++ pyqtlet2/leaflet/layer/marker/marker.py
@@ -5,26 +5,38 @@ from PyQt5.QtCore import pyqtSlot, pyqtSignal, QJsonValue
 class Marker(Layer):
     moveend = pyqtSignal(dict)
     move = pyqtSignal(dict)
+    click = pyqtSignal(dict)
 
     def __init__(self, latLng, options=None):
         super().__init__()
         self.latLng = latLng
         self.options = options
+        self.opacity = options.get('opacity', 0)
         self._initJs()
         self._connectEventToSignal('move', '_onMove')
         self._connectEventToSignal('moveend', '_onMoveend')
+        self._connectEventToSignal('click', '_click')
 
     @pyqtSlot(QJsonValue)
     def _onMove(self, event):
-        self._logger.debug('map clicked. event: {event}'.format(event=event))
+        self._logger.debug('marker moved. event: {event}'.format(event=event))
         event = self._qJsonValueToDict(event)
         self.latLng = [event["latlng"]["lat"], event["latlng"]["lng"]]
         self.move.emit(event)
 
     @pyqtSlot(QJsonValue)
     def _onMoveend(self, event):
-        self._logger.debug('map clicked. event: {event}'.format(event=event))
-        self.moveend.emit({**self._qJsonValueToDict(event), "sender": self})
+        self._logger.debug('marker moved. event: {event}'.format(event=event))
+        if self.opacity == 0:
+            return
+        self.moveend.emit({**self._qJsonValueToDict(event), "latLng": self.latLng, "sender": self})
+
+    @pyqtSlot(QJsonValue)
+    def _click(self, event):
+        self._logger.debug('marker clicked. event: {event}'.format(event=event))
+        if self.opacity == 0:
+            return
+        self.click.emit({**self._qJsonValueToDict(event), "sender": self})
 
     def _initJs(self):
         leafletJsObject = 'L.marker({latLng}'.format(latLng=self.latLng)
@@ -39,6 +51,7 @@ class Marker(Layer):
         self.runJavaScript(js)
 
     def setOpacity(self, opacity):
-        js = '{layerName}.setOpacity({latLng})'.format(
-                layerName=self._layerName, opacity=opacity)
+        self.opacity = opacity
+        js = '{layerName}.setOpacity({opacity})'.format(
+                layerName=self._layerName, opacity=self.opacity)
         self.runJavaScript(js)
