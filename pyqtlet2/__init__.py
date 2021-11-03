e11a0d96ee9745fc3ba0b6adf886b8b993e5207f
Leon Friedmann <leon@friedmann.eu>
2021-11-03 21:09:16 +0100

    Version bump 0.4.9

--- pyqtlet2/__init__.py
+++ pyqtlet2/__init__.py
@@ -3,7 +3,7 @@ Bringing Leaflet maps to PyQt.
 """
 
 __author__ = 'Leon Friedmann <leon.friedmann@tum.de>'
-__version__ = '0.4.8'
+__version__ = '0.4.9'
 
 from .mapwidget import MapWidget
 from .leaflet import L
