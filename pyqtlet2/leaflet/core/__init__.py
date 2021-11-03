2462d5950df634f7a14215a71d8c29c84f23f06e
Leon Friedmann <leon@friedmann.eu>
2021-11-03 23:19:39 +0100

    Added Parser + Unit tests (#9)
    ==Done:
    - Implemented a parser which can parse dicts to the js format which is needed (and back)
    - Added tests for marker, polyline as well as the new introduced parser

--- pyqtlet2/leaflet/core/__init__.py
+++ pyqtlet2/leaflet/core/__init__.py
@@ -1,1 +1,2 @@
 from .evented import Evented
+
---
+++ tests/test_layer_elements.py
@@ -0,0 +1,34 @@
+import unittest
+import sys
+from pyqtlet2 import L, MapWidget
+from PyQt5.QtWidgets import QApplication
+
+app = QApplication(sys.argv)
+
+
+class LayerTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.mapWidget = MapWidget()
+        cls.map = L.map(cls.mapWidget)
+        cls.initial_latlng = (0, 0)
+        cls.new_latlng = (1, 1)
+        cls.initial_layer_latlngs = [(0, 0), (1, 1)]
+
+    def test_marker(self):
+        test_marker = L.marker(self.initial_latlng, options={"draggable": False})
+        test_marker.setLatLng(self.new_latlng)
+        self.assertEqual(test_marker.latLng, self.new_latlng)
+
+        self.assertFalse(test_marker.draggable)
+        test_marker.setDragging(True)
+        self.assertTrue(test_marker.draggable)
+
+    def test_polyline(self):
+        test_polyline = L.polyline(self.initial_layer_latlngs, options=None)
+        self.assertEqual(test_polyline.latLngs, self.initial_layer_latlngs)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
