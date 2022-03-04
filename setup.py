1f314084aebdf944330a6ba1f416a6b382243333
danielhrisca <daniel.hrisca@gmail.com>
2022-03-04 12:17:53 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     },
     install_requires=[
         'QtPy>=2.0.1'
-    ]
+    ],
     extras_require = {
         'PyQt5' : ['PyQt5>=5.15.5','PyQtWebEngine>=5.15.5'],
         'PyQt6' : ['PyQt6>=6.2.0','PyQt6-WebEngine>=6.2.0'],
