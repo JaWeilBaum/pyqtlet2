e6c14b2c27ab3385b6b40a429b3b589929e60549
Leon Friedmann <leon@friedmann.eu>
2022-09-30 12:04:37 +0200

    PyPi version 0.8.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='pyqtlet2',
-    version='0.8.0',
+    version='0.8.1',
     description='Bringing leaflet maps to Python Qt bindings',
     long_description=long_description,
     long_description_content_type="text/markdown",
