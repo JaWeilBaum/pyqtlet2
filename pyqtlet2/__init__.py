c5ee20f914de38ef7f302a06bda6a6796515ada1
Leon Friedmann <leon@friedmann.eu>
2022-12-28 21:27:09 +0100

    Version 0.9.1 - adjusted _onMove to _onMoveend body

--- pyqtlet2/__init__.py
+++ pyqtlet2/__init__.py
@@ -3,7 +3,7 @@ Bringing Leaflet maps to PyQt.
 """
 
 __author__ = 'Leon Friedmann <leon.friedmann@tum.de>'
-__version__ = '0.9.0'
+__version__ = '0.9.1'
 
 from .mapwidget import MapWidget
 from .leaflet import L
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='pyqtlet2',
-    version='0.9.0',
+    version='0.9.1',
     description='Bringing leaflet maps to Python Qt bindings',
     long_description=long_description,
     long_description_content_type="text/markdown",
