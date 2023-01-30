975123cb56bf9ca9e1669e2461e74900b152d863
Leon Friedmann <leon@friedmann.eu>
2023-01-30 11:53:21 +0100

    Version bump 0.9.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='pyqtlet2',
-    version='0.9.1',
+    version='0.9.2',
     description='Bringing leaflet maps to Python Qt bindings',
     long_description=long_description,
     long_description_content_type="text/markdown",
